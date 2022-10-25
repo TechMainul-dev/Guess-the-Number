138d67e9378ef82cae647e54461a7a350c581244
Mainul Hasan Khan <72866491+TechMainul-dev@users.noreply.github.com>
2022-10-25 05:09:32 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const randomNum = Math.floor((Math.random() * (10 - 0) + 1));
+let newNum;
+let i = 0;
+do {
+  newNum = +prompt('Guess a number (1 to 10)');
+  i++
+
+  if (randomNum < newNum) {
+    console.log(`⚠️! your number`, newNum, `is too big    >>>>\n`);
+  } else if (randomNum > newNum) {
+    console.log(`⚠️! your number`, newNum, `is too small  <<<<\n`);
+  }
+} while (randomNum != newNum);
+console.log(`\nCongrats 👑! the number was exact`, randomNum, `& you tried`, i, `times`);
